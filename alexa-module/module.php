716c1934448e8ba56147c7b1dbbdce64e554c69b
daniel.mueller <daniel.mueller@osthus.com>
2017-01-02 15:23:03 +0100

    script and hook generation

--- alexa-module/module.php
+++ alexa-module/module.php
@@ -22,17 +22,65 @@
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
+            
+            $sid = $this->RegisterScript("AlexaHook", "Alexa Hook", "<?\n\nAlexa_handleRequest($_GET);\n\n?>");
+			$this->RegisterHook("/hook/alexa", $sid);
         }
- 
+        
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
         *
-        * ABC_MeineErsteEigeneFunktion($id);
+        * Alexa_handleDiscoverRequest($id);
         *
         */
-        public function MeineErsteEigeneFunktion() {
-            // Selbsterstellter Code
+        public function handleRequest($get) {
+            $deviceId = $get['action'];            
+            
+            if ( "discover" == $action ) {
+                return $this->handleDiscoverRequest();
+            } else if ( "control" == $action ) {
+                return $this->handleControlRequest($get['id'], $get['value']);
+            } else if ( "health" == $action ) {
+                return $this->handleHealthRequest();
+            }
         }
+ 
+
+        private function handleDiscoverRequest() {
+            //TODO
+        }
+        
+        private function handleHealthRequest() {
+            //TODO
+        }
+        
+        private function handleControlRequest($deviceId, $value) {
+            //TODO
+        }
+        
+        /**
+        * Following function is was original developed by Paresy: https://github.com/paresy/SymconMisc/blob/master/EgiGeoZone/module.php        
+        */        
+        private function RegisterHook($HookEndpoint, $TargetID) {
+			$ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
+			if(sizeof($ids) > 0) {
+				$hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
+				$found = false;
+				foreach($hooks as $index => $hook) {
+					if($hook['Hook'] == $HookEndpoint) {
+						if($hook['TargetID'] == $TargetID)
+							return;
+						$hooks[$index]['TargetID'] = $TargetID;
+						$found = true;
+					}
+				}
+				if(!$found) {
+					$hooks[] = Array("Hook" => $HookEndpoint, "TargetID" => $TargetID);
+				}
+				IPS_SetProperty($ids[0], "Hooks", json_encode($hooks));
+				IPS_ApplyChanges($ids[0]);
+			}
+		}        
     }
 ?>
